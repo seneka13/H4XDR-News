314e9bd4af97a70d2c5d9d677b1d301922384f6d
seneka <samat_musaev@epam.com>
2022-08-25 15:07:05 +0600

    Initial Commit

---
+++ H4XDR News/H4XDR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XDR_NewsApp.swift
+//  H4XDR News
+//
+//  Created by Samat Musaev on 25/8/22.
+//
+
+import SwiftUI
+
+@main
+struct H4XDR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
